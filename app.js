e880743317e2d68684b36d90d049b0fb79ef7f98
Ezequiel de Oliveira <eopnetto@gmail.com>
2018-06-03 19:36:02 -0300

    - first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function dropdownMenu() {
+    let dropdownClick = document.getElementById('dropdownClick')
+
+    if (dropdownClick.className === 'topnav') {
+        dropdownClick.className += ' responsive'
+    } else {
+        dropdownClick.className = 'topnav'
+    }
+}
